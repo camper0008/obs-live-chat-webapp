f47c6246a1a76fe4f5f5ed01d3eb04ce06b9985f
camper0008 <spetakel123@gmail.com>
2022-01-05 12:38:27 +0100

    updated to reflect back.js renaming

--- start.bat
+++ start.bat
@@ -3,4 +3,4 @@ set /p domain="Enter domain name (ex. teamslivechat): "
 
 echo Server is now starting. Use browser source in OBS with ip of "127.0.0.1:80/local" to display chat.
 start /b cmd /k "title Tunnel & npx localtunnel --port 80 --subdomain %domain%"
-start /b cmd /k "title Local Server & node ./server/back.js"
+start /b cmd /k "title Local Server & node ./server.js"
